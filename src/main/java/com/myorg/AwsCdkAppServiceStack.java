95c5743b3b06d655e25348d4432bcd25d5c61371
AderisMartins <aderismartins.f@gmaiil.com>
2022-03-13 17:26:46 -0300

    VPC created with success

--- src/main/java/com/myorg/AwsCdkAppServiceStack.java
+++ src/main/java/com/myorg/AwsCdkAppServiceStack.java
@@ -1,10 +1,8 @@
 package com.myorg;
 
 import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
 
 public class AwsCdkAppServiceStack extends Stack {
     public AwsCdkAppServiceStack(final Construct scope, final String id) {
@@ -14,11 +12,5 @@ public class AwsCdkAppServiceStack extends Stack {
     public AwsCdkAppServiceStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsCdkAppServiceQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
     }
 }
---
+++ src/main/java/com/myorg/VpcStk.java
@@ -0,0 +1,21 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class VpcStk extends Stack{
+    public VpcStk(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStk(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        Vpc.Builder.create(this, "Vpc1")
+                .maxAzs(3)
+                .natGateways(0)
+                .build();
+    }
+}
